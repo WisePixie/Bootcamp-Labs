f9f5d013a5dcb172e15bc843df753311073fd96d
Koren P <wisepixie00@gmail.com>
2024-05-07 16:27:43 -0400

    adding basicLoops

---
+++ basicLoops/basicLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿string choice;
+
+do
+{
+    Console.WriteLine("Hello, World!");
+
+    Console.Write("Would you like to continue? (y/n): ");
+    choice = Console.ReadLine();
+}
+while (choice.ToLower() == "y");
+
+Console.WriteLine("Goodbye!");
