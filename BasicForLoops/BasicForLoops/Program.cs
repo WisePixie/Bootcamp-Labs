51e14b0415abcce0f9af8907a6c4517e3fabc6d3
Koren P <wisepixie00@gmail.com>
2024-05-13 16:11:11 -0400

    new lab

---
+++ BasicForLoops/BasicForLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿string choice;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+
+            Console.Write("Would you like to continue? (y/n): ");
+            choice = Console.ReadLine();
+            Console.WriteLine();
+        }
+        while (choice.ToLower() == "y");
+
+        Console.WriteLine("Goodbye!");
+
